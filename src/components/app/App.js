1ff0e9cbf956a90261a69b4a12304a3671f4c635
Mario Quintana <pyrros2000@gmail.com>
2019-01-30 12:28:42 -0800

    Basic app loads

---
+++ src/components/app/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from '../home/Home';
+// import Header from '../header/Header';
+// import About from '../about/About';
+// import Projects from '../projects/Projects';
+// import Contact from '../contact/Contact';
+// import Footer from '../footer/Footer';
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <Home/>
+      </div>
+    );
+  }
+}
+
+export default App;
