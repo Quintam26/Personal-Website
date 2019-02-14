cf3b87f9001cd0cdaf01444c5ff81ebe34daa94b
Mario Quintana <pyrros2000@gmail.com>
2019-02-14 14:32:30 -0800

    small updates to try favicon

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -15,12 +15,12 @@ class About extends Component {
                 <div className="item">
                   <div className="bio">
                     <h1>About</h1>
-                    <p>Mario born and raised in Parkdale, Oregon was molded by the extreme working conditions in the orchards his family and he harvested for many years. His tenacious nature can be seen by the discipline that defines his personal development. Following a rigorous powerlifting program, adhering to a unmerciful diet, reading books in philosophy, inernational development, history, and the classics Mario seeks to master himself.</p>
+                    <p>Mario born and raised in Parkdale, Oregon was molded by the extreme working conditions in the orchards his family and he harvested for many years. His tenacious nature can be seen by the discipline that defines his personal development. Following a rigorous powerlifting program, adhering to a unmerciful diet, reading books in philosophy, international development, history and the classics Mario seeks to master himself.</p>
                   </div>
                   <br/>
                   <div className="skl">
                     <h1>Skills</h1>
-                    <p><strong>Languages</strong> - JavaScript,HTML/CSS <strong>Frameworks/Libraries</strong> - React, Jest, Redux, NodeJS, Express, Mocha, Chai, Mongoose <strong>Databases</strong> - MongoDB, Firebase <strong>Tools</strong> - Heroku, Git, GitHub <strong>Dev Practices</strong> - TDD/BDD <strong>Bilingual</strong> - English, Spanish</p>
+                    <p><strong>Languages</strong> - JavaScript,HTML/CSS <strong>Frameworks/Libraries</strong> - React, Jest, Redux, NodeJS, Express, Mocha, Chai, Mongoose <strong>Databases</strong> - MongoDB, Firebase, PostgreSQL <strong>Tools</strong> - Heroku, Git, GitHub <strong>Dev Practices</strong> - TDD/BDD <strong>Bilingual</strong> - English, Spanish</p>
                   </div>
                 </div>
               </div>
