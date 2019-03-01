e3123ed59705b302f9d7ea2094352c39cbc3567e
Mario Quintana <pyrros2000@gmail.com>
2019-02-28 17:07:58 -0800

    Updated about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -15,7 +15,8 @@ class About extends Component {
                 <div className="item">
                   <div className="bio">
                     <h1>About</h1>
-                    <p>Born and raised in Oregon, I had a humble upbringing harvesting for many years with my family the orchards of Hood River. With time I was exposed to technology through video games, computers at school, and the rise of the internet. As technology improved I found myself watching TechTV, reading technology magazines, and consuming any information. However,one day I found a online tutorial on how to build a webpage. I created a crude webpage of my favorite car and for the first time I realized that I wanted to be a developer. I spend my free time training as powerlifter and reading as well.</p>
+                    <p>Born and raised in Oregon I had a humble upbringing and for many years my family and I worked in the orchards of Hood River. With time I was exposed to technology through video games, the media, computers at school, and the rise of the internet. As technology improved I found myself watching TechTV, reading technology magazines, and consuming any information.</p><br/> 
+                    <p>However, one day I found a online tutorial on how to build a webpage. I created a crude webpage of my favorite car and for the first time I realized that I wanted to be a developer. Since that day I have committed to improving as a developer and enjoy learning new technologies. During my free time I train as powerlifter and read as well.</p>
                   </div>
                 </div>
               </div>
